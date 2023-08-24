0372771da2c808697916dfba33ae7ca05b93c801
Domen Kožar <domen@dev.si>
2023-08-24 16:12:13 +0100

    frontend: add upload icon

--- frontend/src/Pages/Home_.elm
+++ frontend/src/Pages/Home_.elm
@@ -2,6 +2,8 @@ module Pages.Home_ exposing (page)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Svg
+import Svg.Attributes as SvgAttr
 import View exposing (View)
 
 
@@ -39,8 +41,23 @@ page =
                         , div [ class "flex items-center space-x-2" ]
                             [ input [ class "py-1 px-2 mx-8 rounded", placeholder "Search", type_ "text" ]
                                 []
-                            , button [ class "bg-blue-500 text-white py-1 px-4 rounded hover:bg-blue-600" ]
-                                [ text "Publish" ]
+                            , button [ class "bg-blue-950 text-white py-1 px-4 rounded hover:bg-blue-600" ]
+                                [ Svg.svg
+                                    [ SvgAttr.fill "none"
+                                    , SvgAttr.viewBox "0 0 24 24"
+                                    , SvgAttr.strokeWidth "1.5"
+                                    , SvgAttr.stroke "currentColor"
+                                    , SvgAttr.class "w-5 h-5"
+                                    ]
+                                    [ Svg.path
+                                        [ SvgAttr.strokeLinecap "round"
+                                        , SvgAttr.strokeLinejoin "round"
+                                        , SvgAttr.d "M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                                        ]
+                                        []
+                                    ]
+                                , text "Publish"
+                                ]
                             ]
                         ]
                     ]
@@ -48,7 +65,7 @@ page =
             ]
         , {- Content -}
           main_
-            [ class "container w-auto bg-slate-50 p-24 h-full flex-1"
+            [ class "container w-auto bg-slate-50 p-24 h-full"
             ]
             [ h1
                 [ class "text-3xl font-semibold mb-4"
