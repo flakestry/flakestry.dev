b349d7311a891988fc66a1760d3ab8298561c44f
Sander <hey@sandydoo.me>
2023-08-30 13:59:56 +0000

    Link the entire flake card to the repo page

--- frontend/src/Components/FlakeCard.elm
+++ frontend/src/Components/FlakeCard.elm
@@ -17,14 +17,28 @@ type alias FlakeCard =
 
 view : FlakeCard -> Html msg
 view card =
-    div [ class "mx-auto text-lg block bg-white w-full h-28 px-8 py-6 rounded-md shadow-sm border border-slate-150" ]
-        [ div [ class "flex items-center" ]
-            [ Octicons.defaultOptions |> Octicons.color "currentColor" |> Octicons.class "inline" |> Octicons.markGithub
-            , a [ Route.Path.href (Route.Path.Flake_Github_Org_ { org = card.username }), class "ml-2 hover:text-sky-500" ] [ span [ class "font-semibold" ] [ text card.username ] ]
-            , span [ class "mx-2" ] [ text "/" ]
-            , a [ Route.Path.href (Route.Path.Flake_Github_Org__Repo_ { org = card.username, repo = card.repo }), class "hover:text-sky-500" ] [ span [ class "font-semibold" ] [ text card.repo ] ]
-            , span [ class "ml-3 mr-1" ] [ Octicons.defaultOptions |> Octicons.color "currentColor" |> Octicons.class "inline" |> Octicons.tag ]
-            , a [ Route.Path.href (Route.Path.Flake_Github_Org__Repo__Version_ { org = card.username, repo = card.repo, version = card.version }), class "hover:text-sky-500" ] [ span [ class "font-semibold" ] [ text card.version ] ]
+    a
+        [ class "relative mx-auto w-full overflow-hidden text-lg block bg-white rounded-md shadow-sm border border-slate-150 transition hover:border-transparent hover:ring-sky-500 hover:ring-2"
+        , Route.Path.href (Route.Path.Flake_Github_Org__Repo__Version_ { org = card.username, repo = card.repo, version = card.version })
+        ]
+        [ div
+            [ class "relative px-4 pt-4 pb-6 md:px-8" ]
+            [ div []
+                [ div [ class "inline-flex items-center flex-wrap" ]
+                    [ span [ class "inline-flex items-center mr-4" ]
+                        [ Octicons.defaultOptions |> Octicons.color "currentColor" |> Octicons.class "inline" |> Octicons.markGithub
+                        , h3 [ class "ml-2 font-semibold truncate" ]
+                            [ span [] [ text card.username ]
+                            , span [ class "mx-1" ] [ text "/" ]
+                            , span [] [ text card.repo ]
+                            ]
+                        ]
+                    , span [ class "my-2 whitespace-nowrap bg-slate-100 rounded p-1 text-sm font-medium" ]
+                        [ span [] [ Octicons.defaultOptions |> Octicons.color "currentColor" |> Octicons.size 14 |> Octicons.class "inline" |> Octicons.tag ]
+                        , span [ class "ml-1" ] [ text card.version ]
+                        ]
+                    ]
+                ]
+            , p [ class "mt-2 truncate" ] [ text card.description ]
             ]
-        , p [ class "mt-2" ] [ text card.description ]
         ]
