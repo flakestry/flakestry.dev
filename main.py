e5439a8bc2b8c7be03712125b8132fca447ef651
Domen Kožar <domen@dev.si>
2023-09-01 13:51:10 +0100

    verify oidc

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from typing import Callable
+from fastapi import FastAPI, Depends
+from fastapi_oidc import IDToken
+from fastapi_oidc import get_auth
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from distutils.version import StrictVersion
 
 app = FastAPI()
-
+FastAPIInstrumentor.instrument_app(app)
 
 # GitHub Org
 #- slug
@@ -18,42 +21,40 @@ app = FastAPI()
 #- revision
 #- readme
 
-OIDC_config = {
-    "client_id": "",
+OIDCConfig = {
+    "client_id": "6779ef20e75817b79602",
     # Audience can be omitted in which case the aud value defaults to client_id
-    "audience": "https://yourapi.url.com/api",
-    "base_authorization_server_uri": "https://dev-126594.okta.com",
-    "issuer": "https://token.actions.githubusercontent.com",
+    #"audience": "https://yourapi.url.com/api",
+    "base_authorization_server_uri": "https://token.actions.githubusercontent.com",
+    "issuer": "token.actions.githubusercontent.com",
     "signature_cache_ttl": 3600,
 }
 
-@app.get("/flakes")
+authenticate_user: Callable = get_auth(**OIDCConfig)
+
+@app.get("/flake")
 def get_flakes():
     return {"flakes": [
 
     ]}
 
-@app.get("/search")
-def read_search():
-    return {"flakes": [
-
-    ]}
 
 @app.get("/flake/{org}")
 def read_org(org: str):
-    return {}
+    return {
+        "flakes": []
+    }
 
 @app.get("/flake/{org}/{repo}")
 def read_repo(org: str, repo: str):
     
     versions = ["1.1.2", "1.0.0", "1.3.3", "1.0.12", "1.0.2"]
     versions.sort(key=StrictVersion)
+    latest = versions[-1]
     
-    return { versions: versions }
+    return { versions: versions, latest: latest}
 
-@app.get("/publish")
-def read_item(version: str):
-    # verify OIDC
+@app.post("/publish")
+def read_item(id_token: IDToken = Depends(authenticate_user)):
+    print(id_token)
     return {}
-
-FastAPIInstrumentor.instrument_app(app)
