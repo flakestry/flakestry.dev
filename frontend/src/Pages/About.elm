f16650f4a8f47e6eec8f892c9cd9d55e55248f8a
Domen Kožar <domen@dev.si>
2023-09-15 10:51:29 +0100

    add about text

--- frontend/src/Pages/About.elm
+++ frontend/src/Pages/About.elm
@@ -3,7 +3,24 @@ module Pages.About exposing (..)
 import Flakestry.Layout
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Markdown
 import View exposing (View)
+
+
+about : String
+about =
+    """
+  Flakestry is [a open source Nix community project](https://github.com/flakestry/flakestry.dev)
+  with the goal to replace the existing https://search.nixos.org/flakes by making it easier to
+  release flakes.
+
+  Currently it's possible to publish flakes via GitHub actions, but other sources could be added later on.
+
+  As flakes stabilize, we hope to see more and more flakes being published to the registry.
+
+  As flakes evolve, we hope to see better introspection of the outputs for each release and upstreaming of
+  important features as flake versioning and dependency resolution.
+"""
 
 
 page : View msg
@@ -12,7 +29,7 @@ page =
     , body =
         Flakestry.Layout.viewBody
             [ Flakestry.Layout.viewNav
-            , main_ [] []
+            , main_ [] [ Markdown.toHtml [] about ]
             , Flakestry.Layout.viewFooter
             ]
     }
