648268fded74ad96b61955e444820870a8d354dd
Silvan Mosberger <silvan.mosberger@tweag.io>
2023-09-25 19:52:24 +0200

    about: Mention initial development

--- frontend/src/Pages/About.elm
+++ frontend/src/Pages/About.elm
@@ -16,6 +16,10 @@ about =
   It is intended as a replacement for the existing [search.nixos.org/flakes](https://search.nixos.org/flakes).
   We are fully committed to making Flakestry an official Nix project.
 
+  Flakestry is an initiative by [Domen Kožar](https://github.com/domenkozar), founder of [Cachix](https://www.cachix.org/), as a response to the comparable but proprietary FlakeHub project.
+  It was initially developed by Domen Kožar and [Sander](https://github.com/sandydoo),
+  with endorsement and input from community members [@RaitoBezarius](https://github.com/RaitoBezarius), [@roberth](https://github.com/roberth), [@zimbatm](https://github.com/zimbatm), [@infinisil](https://github.com/infinisil), [@Kranzes](https://github.com/Kranzes) and [@garbas](https://github.com/garbas).
+
   Currently it's possible to publish flakes via [GitHub actions](/publish), but [other sources could be added](https://github.com/flakestry/flakestry.dev/issues/1) later on.
 
   As flakes stabilize, we hope to see more and more flakes being published to the registry.
