d39834fde6d886010e53dbc7b122d59c5bdc032c
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-04-19 22:12:45 -0500

    Basic route setup for /api

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,9 +1,15 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let api = Router::new()
+        .route("/flake", get(|| async { "Hello, World!" }))
+        .route("/publish", post(|| async { "Hello, World!" }));
+    let app = Router::new().nest("/api", api);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
