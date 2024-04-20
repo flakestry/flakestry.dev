ee3982eb30d9cec7f9a24c1d24c848f25404a9f9
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-04-19 21:46:29 -0500

    Axum hello world example

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
