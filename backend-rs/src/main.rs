3cced06d2cc899486d88f41585a0d9011d24a04f
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-04-21 11:47:12 -0500

    Extract handlers to separate functions

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -7,11 +7,19 @@ use axum::{
 async fn main() {
     // build our application with a single route
     let api = Router::new()
-        .route("/flake", get(|| async { "Hello, World!" }))
-        .route("/publish", post(|| async { "Hello, World!" }));
+        .route("/flake", get(get_flake))
+        .route("/publish", post(post_publish));
     let app = Router::new().nest("/api", api);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_flake() -> &'static str {
+    "Flake"
+}
+
+async fn post_publish() -> &'static str {
+    "Publish"
 }
